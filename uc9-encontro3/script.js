b67ed9b2a7192887e4cabf6c40a63d32036f7ee4
Laura Denardi <88340931+LauraDenardi@users.noreply.github.com>
2021-10-20 22:28:10 -0300

    add modal

--- uc9-encontro3/script.js
+++ uc9-encontro3/script.js
@@ -1,13 +1,13 @@
-function eventoClick(e){
+function eventoClick(e) {
     e.stopPropagation();
     alert("O elemento " + this.nodeName + " foi clicado!")
 }
 
-function eventoFocus(){
+function eventoFocus() {
     let elePai = this.parentNode
     let eleLbl = elePai.querySelector("label")
 
-    if(eleLbl==null){
+    if (eleLbl == null) {
         eleLbl = document.createElement("label")
         elePai.appendChild(eleLbl)
     }
@@ -16,23 +16,42 @@ function eventoFocus(){
     this.style.backgroundColor = "burlywood"
 }
 
-function eventoBlur(){
-    this.style.backgroundColor="white"
+function eventoBlur() {
+    this.style.backgroundColor = "white"
 }
 
-function Funcaoonload(){
+function Funcaoonload() {
     alert("Formulário carregado com sucesso!")
 }
+
+function mostrarModal() {
+    document.querySelector(".modal").style.display = "block"
+    window.location.href = "#"
+    document.body.style.overflow = "hidden"
+}
+
+function fechaModal() {
+    document.querySelector(".modal").style.display = "none"
+    window.location.href = "#"
+    document.body.style.overflow = "visible"
+}
+
 
 
 var els = document.querySelectorAll(".clicaveis")
 
-els.forEach(it=>{
+els.forEach(it => {
     it.addEventListener("click", eventoClick)
 })
 
-els=document.querySelectorAll(".focus-habilitado")
-els.forEach(it=>{
-  it.addEventListener("focus",eventoFocus)
-  it.addEventListener("blur",eventoBlur)
+els = document.querySelectorAll(".focus-habilitado")
+els.forEach(it => {
+    it.addEventListener("focus", eventoFocus)
+    it.addEventListener("blur", eventoBlur)
 })
+
+let itMd = document.querySelector(".click-modal")
+itMd.addEventListener("click", mostrarModal)
+
+let idMd2=document.querySelector(".fecha-menu")
+idMd2.addEventListener("click",fechaModal)
